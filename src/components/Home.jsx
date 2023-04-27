d42a06d311324e266c1e10bf985ba24a55477d4f
Subashvel <srsvel2000@gmail.com>
2023-04-27 01:36:04 -0700

    picture

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
           <img
             src={HeroImage}
             alt="profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-2xl mx-auto w-3/4 md:w-full"
           />
         </div>
         <div className="flex flex-col justify-center h-full">
